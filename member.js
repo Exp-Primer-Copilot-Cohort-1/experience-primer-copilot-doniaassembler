f7daacd32ed28e2815a822ffbdffc50e7f2fa074
doniaassembler <csfernandez@frba.utn.edu.ar>
2024-01-13 00:59:16 -0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,39 @@
+function skillsMember() {
+  return {
+    name: 'DoniaAssembler',
+    skills: {
+      'Assembly': 1,
+      'C': 1,
+      'C++': 1,
+      'C#': 1,
+      'Java': 1,
+      'JavaScript': 1,
+      'Python': 1,
+      'Ruby': 1,
+      'Rust': 1,
+      'TypeScript': 1,
+      'WebAssembly': 1,
+      'x86 Assembly': 1,
+    },
+    projects: [
+      {
+        name: 'experience-primer-copilot-doniaassembler',
+        description: 'A primer on how to use GitHub Copilot to write code.',
+        skills: {
+          'Assembly': 1,
+          'C': 1,
+          'C++': 1,
+          'C#': 1,
+          'Java': 1,
+          'JavaScript': 1,
+          'Python': 1,
+          'Ruby': 1,
+          'Rust': 1,
+          'TypeScript': 1,
+          'WebAssembly': 1,
+          'x86 Assembly': 1,
+        },
+      },
+    ],
+  };
+}
